0e5106706292253128316f620c86b93023dbe5bd
esckeyvirtualescaperooms <85531122+esckeyvirtualescaperooms@users.noreply.github.com>
2021-12-30 22:56:47 -0600

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1302997.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 64364758, 64364755, 64369955 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/64365002/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/64365003/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/64365001/1/ammo.js', 'preload' : true},
+];
